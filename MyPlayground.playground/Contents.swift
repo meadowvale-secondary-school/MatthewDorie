5f0e7b857b2ff56af4a87d210b4b7659910cc94f
Matthew Dorie <588067@pdsb.net>
2019-03-20 13:17:10 -0400

    Initial Commit

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,53 @@
+import UIKit
+
+
+let name = "john"
+print(name)
+
+var age = 29
+age = 30
+print(age)
+
+let defaultScore = 100
+var playerOneScore = defaultScore
+var playerTwoScore = defaultScore
+
+print(playerOneScore)
+print(playerTwoScore)
+
+playerOneScore = 200
+print(playerOneScore)
+
+/* The digits of pi are infinite ,
+ so instead I chose a close approximation.*/
+let π = 3.14
+
+struct Person {
+    let firstName: String
+    let lastName: String
+    
+    func sayHello() {
+        print("Hello there! My name is \(firstName)\(lastName).")
+    }
+}
+
+let aPerson = Person(firstName: "jacob", lastName: "Edwards")
+let anotherPerson = Person(firstName: "Candace", lastName:
+"Salinas")
+
+aPerson.sayHello()
+anotherPerson.sayHello()
+
+let playerName = "julian"
+var playerScore = 1000
+var gameOver = false
+
+var largeUglyNumber = 10000000
+var largeprettyNumber = 1_000_000_000
+
+let cityName = "San Francisco"
+/* "San Francisco" is obviously a 'string', so the compiler
+ automatically assgns the tye of cityname to a 'string' */
+let pi = 3.1415927
+/* 3.14 is a number with decimal points, so the compiler
+ auromatically assigns the type 'pi' to a 'double' */
---
+++ MyPlayground2.playground/Contents.swift
@@ -0,0 +1,40 @@
+import UIKit
+
+var str = "Hello, playground"
+
+let favoritePerson = "Luke"
+var shoeSize = 8
+shoeSize = 9 //Reassigns shoeSize to 9
+
+var opponentScore = 3 * 8
+// opponentScore has a value of 24
+var myScore = 100/4
+// myScore has avalue of 25
+
+var totalScore = opponentScore + myScore
+// totalScore has a value of 49
+myScore = myScore + 3
+//updates myScore to the current value plus 3
+
+let totalDistance = 3.9
+var distanceTravelled = 1.2
+var remainingDistance = totalDistance - distanceTravelled
+// remainingDistance has a value of 2.7
+
+let x = 51
+let y = 4
+let z = x / y // z has a value of 12
+
+var myScore2 = 10
+myScore2 = myScore2 + 3
+// updates myScoreto the current value plus 3
+
+myScore2 += 3 // Adds 3 to myScore
+myScore2 -= 5 // Subtracts 5 from myScore2
+myScore2 *= 2 // Multiples myScore by 2
+myScore2 /= 2 // Divides myScore by 2
+
+var u = 2
+var i = 3
+var o = 5
+
