69a7d4a6b5d3324395b3644ec2ba7422b6e793ed
Matthew Dorie <588067@pdsb.net>
2019-03-21 13:14:38 -0400

    Updated Save

--- protocolsLesson4.1.playground/Contents.swift
+++ protocolsLesson4.1.playground/Contents.swift
@@ -32,4 +32,19 @@ class Shoe2: CustomStringConvertible {
     let color: String = ""
     let size: Int = 0
     let hasLaces: Bool
+    
+    init(color: String, size: Int, hasLaces: Bool) {
+        self.color = color
+        self.size = size
+        self.hasLaces = hasLaces
+    }
+    
+    var description: String {
+        return "Shoe(color: \(color), size: \(size), hasLaces: \
+        (hasLaces))"
+    }
 }
+let  myShoe1 = Shoe2(color: "Black", size: 12, hasLaces: true)
+let yourShoe1 = Shoe2(color: "Red", size: 8, hasLaces: false)
+print(myShoe1)
+print(yourShoe1)
