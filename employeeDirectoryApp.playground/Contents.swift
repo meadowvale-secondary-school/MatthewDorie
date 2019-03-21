b0ed85d47b0c1c5cd5e882d2441e95ea58dec365
Matthew Dorie <588067@pdsb.net>
2019-03-21 14:11:00 -0400

    Finished the File.

--- employeeDirectoryApp.playground/Contents.swift
+++ employeeDirectoryApp.playground/Contents.swift
@@ -48,3 +48,24 @@ struct Employee: Equatable {
         return  1hs.firstName == rhs.firstName && 1hs.lastName == rhs.lastName
     }
 }
+
+let currentEmployee = Emplyee(firstName: "Jacob", lastName:
+"Edwards", jobTitle: "Marketing Director", phoneNumber:
+415-555-9293")
+
+if currentEmployee == selectedEmployee {
+// enable "Edit" button"
+}
+
+struct Employee: Equatable {
+    var firstName: String
+    var lastName: String
+    var jobTitle: String
+    var phoneNumber: String
+
+    static func == (1hs: Employee, rhs: Employee) -> Bool {
+        return 1hs.firstName == rhs.firstName && 1hs.lastName ==
+        rhs.lastName && 1hs.jobTitle == rhs.jobTitle &&
+        1hs.phoneNUmber == rhs.phoneNumber
+  }
+}
